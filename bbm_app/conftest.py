8c40248da0396c132e9b19605885ad356945a2db
HerrMaszewsk <tadmaszewski@gmail.com>
2023-08-05 16:24:08 +0200

    testy przepisane na pytest

---
+++ bbm_app/conftest.py
@@ -0,0 +1,68 @@
+from django.contrib.auth import get_user_model
+from .models import Coach, Race, Position, Team, RacePositionLimit
+import pytest
+
+
+@pytest.fixture
+def client():
+    from django.test import Client
+    return Client()
+
+
+@pytest.fixture
+def registration_data():
+    return {
+        'coach_name': 'test_coach',
+        'password': 'password123',
+        'password2': 'password123'
+    }
+
+
+@pytest.fixture
+def test_user(db):
+    username = 'test_coach'
+    password = 'password123'
+    user = get_user_model().objects.create_user(username=username, password=password)
+    return user, password
+
+
+@pytest.fixture
+def test_coach(test_user):
+    user, _ = test_user
+    return Coach.objects.create(user=user, coach_name=user.username)
+
+
+@pytest.fixture
+def test_race(db):
+    return Race.objects.create(race_type='HUM')
+
+
+@pytest.fixture
+def logged_in_client(client, test_user):
+    username, password = test_user
+    client.login(username=username, password=password)
+    return client
+
+
+@pytest.fixture
+def test_position(test_race): # Assuming you have a test_race fixture
+    return Position.objects.create(
+        name='Test Position',
+        movement=6,
+        strength=3,
+        agility=3,
+        armor=8,
+        passing=2,
+        cost=50000,
+        race=test_race, # Associate with a specific race
+    )
+
+
+@pytest.fixture
+def test_team(test_coach, test_race):
+    return Team.objects.create(coach=test_coach, race=test_race, team_name='Test Team')
+
+
+@pytest.fixture
+def test_race_position_limit(test_race, test_position):
+    return RacePositionLimit.objects.create(race=test_race, position=test_position, max_count=4)
--- bbm_app/tests.py
+++ bbm_app/tests.py
@@ -1,12 +1,11 @@
 import pytest
 from django.urls import reverse
+from django.contrib.auth import get_user_model
 from django.contrib.auth.models import User
 from .models import Coach, Race, Team, Position, RacePositionLimit, Player
-from django.test import TestCase, Client
-from django.contrib.auth import get_user_model
-
 
 # Testy dla LoginView
+
 
 @pytest.mark.django_db
 def test_valid_login(client):
@@ -42,148 +41,111 @@ def test_user_not_coach(client):
 # Testy dla RegistrationView
 
 
-class RegistrationViewTest(TestCase):
-    def setUp(self):
-        self.data = {
-            'coach_name': 'test_coach',
-            'password': 'password123',
-            'password2': 'password123'
-        }
-
-    def test_successful_registration(self):
-        response = self.client.post(reverse('registration'), self.data)
-        self.assertEqual(response.status_code, 302)
-        self.assertRedirects(response, reverse('create_team'))
-        user = User.objects.get(username='test_coach')
-        self.assertIsNotNone(user)
-        self.assertTrue(user.check_password('password123'))
-
-    def test_unsuccessful_registration_invalid_data(self):
-        data = self.data.copy()
-        data['coach_name'] = ''
-        response = self.client.post(reverse('registration'), data)
-        self.assertEqual(response.status_code, 200)
-        form = response.context['form']
-        self.assertFormError(form, 'coach_name', 'This field is required.')
-
-    def test_user_logged_in_after_successful_registration(self):
-        response = self.client.post(reverse('registration'), self.data)
-        self.assertEqual(response.status_code, 302)
-        self.assertRedirects(response, reverse('create_team'))
-        user = User.objects.get(username='test_coach')
-        self.assertIsNotNone(user)
-        self.assertEqual(int(self.client.session['_auth_user_id']), user.pk)
+def test_successful_registration(client, registration_data, db):
+    response = client.post(reverse('registration'), registration_data)
+    assert response.status_code == 302
+    assert response.url == reverse('create_team')
+    user = User.objects.get(username='test_coach')
+    assert user is not None
+    assert user.check_password('password123')
 
 
-class CreateTeamViewTest(TestCase):
-    def setUp(self):
-        self.username = 'test_coach'
-        self.password = 'password123'
-        user = get_user_model().objects.create_user(username=self.username, password=self.password)
-        self.coach = Coach.objects.create(user=user, coach_name=self.username)
-        self.client.login(username=self.username, password=self.password)
-        self.race = Race.objects.create(race_type='HUM')
-
-    def test_render_create_team_form(self):
-        response = self.client.get(reverse('create_team'))
-        self.assertEqual(response.status_code, 200)
-        self.assertTemplateUsed(response, 'create_team.html')
-
-    def test_successful_team_creation(self):
-        data = {
-            'team_name': 'Test Team',
-            'race': self.race.pk,
-        }
-        response = self.client.post(reverse('create_team'), data)
-        self.assertEqual(response.status_code, 302)
-        team = Team.objects.get(team_name='Test Team')
-        self.assertEqual(team.coach, self.coach)
-        self.assertRedirects(response, reverse('manage_team', args=[team.pk]))
-
-    def test_unsuccessful_team_creation(self):
-        data = {
-            'team_name': '',
-            'race': self.race.pk,
-        }
-        response = self.client.post(reverse('create_team'), data)
-        self.assertEqual(response.status_code, 200)
-        form = response.context['form']
-        self.assertFormError(form, 'team_name', 'This field is required.')
+def test_unsuccessful_registration_invalid_data(client, registration_data):
+    registration_data['coach_name'] = ''
+    response = client.post(reverse('registration'), registration_data)
+    assert response.status_code == 200
+    form = response.context['form']
+    assert form.errors['coach_name'] == ['This field is required.']
 
 
-class ManageTeamViewTest(TestCase):
-    def setUp(self):
-        user = get_user_model().objects.create_user(username='test_coach', password='password123')
-        coach = Coach.objects.create(user=user, coach_name='test_coach')
-        self.race = Race.objects.create(race_type='HUM')
-        self.position = Position.objects.create(
-            name='Test Position',
-            movement=6,
-            strength=3,
-            agility=3,
-            armor=8,
-            passing=2,
-            cost=50000,
-        )
-        self.team = Team.objects.create(coach=coach, race=self.race, team_name='Test Team')
-        self.client.login(username='test_coach', password='password123')
-        RacePositionLimit.objects.create(race=self.race, position=self.position, max_count=4)
-
-    def test_create_player_successful(self):
-        data = {
-            'name': 'Player Name',
-            'number': 1,
-            'position': self.position.pk,
-        }
-        response = self.client.post(reverse('manage_team', args=[self.team.pk]), data)
-        self.assertEqual(response.status_code, 302)
-        player = Player.objects.get(name='Player Name')
-        self.assertEqual(player.number, 1)
-        self.assertEqual(player.position, self.position)
-        self.assertEqual(player.player_team, self.team)
-
-    def test_create_player_insufficient_funds(self):
-        self.team.treasury = 0
-        self.team.save()
-
-        data = {
-            'name': 'Player One',
-            'number': 1,
-            'position': self.position.pk
-        }
-        response = self.client.post(reverse('manage_team', kwargs={'team_pk': self.team.pk}), data)
-        self.assertEqual(response.status_code, 200)
-        self.assertContains(response, 'Insufficient funds.')
-
-    def test_create_player_exceeding_position_limit(self):
-        race_position_limit, created = RacePositionLimit.objects.get_or_create(race=self.team.race, position=self.position)
-        race_position_limit.max_count = 0
-        race_position_limit.save()
-
-        player_data = {
-            'name': 'Player One',
-            'number': 1,
-            'position': self.position.pk,
-        }
-        response = self.client.post(reverse('manage_team', kwargs={'team_pk': self.team.pk}), player_data)
-        self.assertEqual(response.status_code, 200)
-        self.assertFalse(Player.objects.filter(name='Player One').exists())
-
-    def test_create_player_invalid_number(self):
-        player_data = {
-            'name': 'Player Four',
-            'number': 17,
-            'position': self.position.pk,
-        }
-        response = self.client.post(reverse('manage_team', kwargs={'team_pk': self.team.pk}), player_data)
-        self.assertEqual(response.status_code, 200)
-        self.assertFalse(Player.objects.filter(name='Player Four').exists())
-        self.assertContains(response, 'Invalid number. Please choose a number between 1 and 16.')
+def test_user_logged_in_after_successful_registration(client, registration_data, db):
+    response = client.post(reverse('registration'), registration_data)
+    assert response.status_code == 302
+    assert response.url == reverse('create_team')
+    user = User.objects.get(username='test_coach')
+    assert user is not None
+    assert int(client.session['_auth_user_id']) == user.pk
 
 
+def test_render_create_team_form(logged_in_client):
+    response = logged_in_client.get(reverse('create_team'))
+    assert response.status_code == 200
+    assert 'create_team.html' in [template.name for template in response.templates]
 
 
+def test_successful_team_creation(logged_in_client, test_coach, test_race):
+    data = {
+        'team_name': 'Test Team',
+        'race': test_race.pk,
+    }
+    response = logged_in_client.post(reverse('create_team'), data)
+    assert response.status_code == 302
+    team = Team.objects.get(team_name='Test Team')
+    assert team.coach == test_coach
+    assert response.url == reverse('manage_team', args=[team.pk])
 
 
+def test_unsuccessful_team_creation(logged_in_client, test_race):
+    data = {
+        'team_name': '',
+        'race': test_race.pk,
+    }
+    response = logged_in_client.post(reverse('create_team'), data)
+    assert response.status_code == 200
+    form = response.context['form']
+    assert form.errors['team_name'] == ['This field is required.']
 
+
+def test_create_player_successful(logged_in_client, test_team, test_position, test_race_position_limit):
+    data = {
+        'name': 'Player Name',
+        'number': 1,
+        'position': test_position.pk,
+    }
+    response = logged_in_client.post(reverse('manage_team', args=[test_team.pk]), data)
+    assert response.status_code == 302
+    player = Player.objects.get(name='Player Name')
+    assert player.number == 1
+    assert player.position == test_position
+    assert player.player_team == test_team
+
+
+def test_create_player_insufficient_funds(logged_in_client, test_team, test_position, test_race_position_limit):
+    test_team.treasury = 0
+    test_team.save()
+
+    data = {
+        'name': 'Player One',
+        'number': 1,
+        'position': test_position.pk
+    }
+    response = logged_in_client.post(reverse('manage_team', kwargs={'team_pk': test_team.pk}), data)
+    form = response.context['form']
+
+    assert 'Insufficient funds.' in str(form.errors['position'])
+
+
+def test_create_player_exceeding_position_limit(logged_in_client, test_race, test_position, test_team):
+    race_position_limit, created = RacePositionLimit.objects.get_or_create(race=test_race, position=test_position, max_count=0)
+
+    player_data = {
+        'name': 'Player One',
+        'number': 1,
+        'position': test_position.pk,
+    }
+    response = logged_in_client.post(reverse('manage_team', kwargs={'team_pk': test_team.pk}), player_data)
+    assert response.status_code == 200
+    assert not Player.objects.filter(name='Player One').exists()
+
+
+def test_create_player_invalid_number(logged_in_client, test_team, test_position):
+    player_data = {
+        'name': 'Player Four',
+        'number': 17,
+        'position': test_position.pk,
+    }
+    response = logged_in_client.post(reverse('manage_team', kwargs={'team_pk': test_team.pk}), player_data)
+    assert response.status_code == 200
+    assert not Player.objects.filter(name='Player Four').exists()
+    assert 'Invalid number. Please choose a number between 1 and 16.' in response.content.decode()
 
